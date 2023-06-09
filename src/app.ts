f8bf1ac2a7157bab7c7b13a4a6ff6660b1cad01f
Harsh Arora <arora99harsh@gmail.com>
2023-06-09 17:44:55 +0530

    Create an autobind decorator to bind this to a method

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,18 @@
 // Code goes here!
+// create a decorator to auto bind the submit handler to 'this'
+// first parameter is target,second is methodName but we used _ to begin the parameter name as we don't have any use of them and we want typescript to 
+// not give any error or we can set noUnusedParameters to false in tsconfig.json
+function autobind(_:any,_2:string,descriptor:PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor :PropertyDescriptor = {
+        configurable:true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    }
+    return adjustedDescriptor;
+} 
 class ProjectInput {
     projectElem: HTMLTemplateElement;
     hostElem: HTMLDivElement;
@@ -22,12 +36,13 @@ class ProjectInput {
         // render form inside app div
         this.attach();
     }
+    @autobind
     private submitHandler(event: Event){
         event.preventDefault();
         console.log(this.titleEle.value);
     }
     private configure(){
-        this.formElem.addEventListener('submit',this.submitHandler.bind(this));
+        this.formElem.addEventListener('submit',this.submitHandler);
     }
     private attach(){
         this.hostElem.insertAdjacentElement('afterbegin',this.formElem);
